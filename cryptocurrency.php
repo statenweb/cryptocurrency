a731d5512a70f317367d23d0a0d83d83d8711aa7
Gargano, Matthew <mgargano@penguinrandomhouse.com>
2017-12-29 19:52:52 -0500

    add i18n

--- cryptocurrency.php
+++ cryptocurrency.php
@@ -6,6 +6,8 @@ Plugin URI: https://statenweb/crytpocurrency
 Author: Mat Gargano
 Version: 0.0.1
 Author URI: https://statenweb.com/
+Text Domain:       cryptocurrency
+Domain Path:       /languages
 */
 
 use Cryptocurrency\Actions;
@@ -13,6 +15,8 @@ use Cryptocurrency\Settings\Global_Settings;
 use Cryptocurrency\Shortcodes\Table;
 
 require __DIR__ . '/vendor/autoload.php';
+
+add_action( 'plugins_loaded', 'cryptocurrency_load_plugin_textdomain' );
 
 $global_settings = new Global_Settings();
 $global_settings->init();
@@ -28,3 +32,8 @@ function cryptocurrency_table( $output = true ) {
 	return \Cryptocurrency\Output\Table::generate( $output );
 
 }
+
+function cryptocurrency_load_plugin_textdomain() {
+	load_plugin_textdomain( 'cryptocurrency', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+
